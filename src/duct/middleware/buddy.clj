3be423915835f30283eafeedbf1afe24388043b9
James Reeves <jreeves@weavejester.com>
2017-12-02 03:41:52 +0000

    Initial commit

---
+++ src/duct/middleware/buddy.clj
@@ -0,0 +1,6 @@
+(ns duct.middleware.buddy)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/duct/middleware/buddy_test.clj
@@ -0,0 +1,7 @@
+(ns duct.middleware.buddy-test
+  (:require [clojure.test :refer :all]
+            [duct.middleware.buddy :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
