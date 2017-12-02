0df463081b9bce395b36be890a51b7ddf482c726
James Reeves <jreeves@weavejester.com>
2017-12-02 07:01:23 +0000

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject duct/middleware.buddy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Duct library for Buddy middleware"
+  :url "https://github.com/duct-framework/middleware.buddy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-RC2"]
