660d894611a863bb88b3fbe42f8890ab6eafe843
James Reeves <jreeves@weavejester.com>
2018-01-05 21:10:47 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/duct-framework/middleware.buddy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [buddy/buddy-auth "2.1.0"]
-                 [duct/core "0.6.1"]
-                 [integrant "0.6.1"]]
+                 [duct/core "0.6.2"]
+                 [integrant "0.6.3"]]
   :profiles
   {:dev {:dependencies [[ring/ring-mock "0.3.2"]]}})
