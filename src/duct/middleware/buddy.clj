861f26717fc6190f2c6d6b2875d1e7d27d127a38
James Reeves <jreeves@weavejester.com>
2018-01-10 20:30:31 +0000

    Add the remaining auth backends

--- src/duct/middleware/buddy.clj
+++ src/duct/middleware/buddy.clj
@@ -4,11 +4,15 @@
             [integrant.core :as ig]))
 
 (def backends
-  {:basic backend/basic})
+  {:basic   backend/basic
+   :session (fn [_] (backend/session))
+   :token   backend/token
+   :jws     backend/jws
+   :jwe     backend/jwe})
 
 (defmethod ig/init-key :duct.middleware.buddy/authentication
   [_ {:keys [backend] :as config}]
   (let [backend-fn (backends backend)
-        config     (dissoc config backend)]
+        config     (dissoc config :backend)]
     (fn [handler]
       (buddy/wrap-authentication handler (backend-fn config)))))
--- test/duct/middleware/buddy_test.clj
+++ test/duct/middleware/buddy_test.clj
@@ -1,24 +1,72 @@
 (ns duct.middleware.buddy-test
-  (:require [clojure.test :refer :all]
+  (:require [buddy.sign.jwt :as jwt]
+            [clojure.test :refer :all]
             [duct.middleware.buddy :as buddy]
             [integrant.core :as ig]
             [ring.mock.request :as mock]))
 
-(defn- test-authfn [_ {:keys [username password]}]
-  username)
+(defn- test-basic-authfn [_ {:keys [username password]}] username)
+(defn- test-token-authfn [_ token] token)
 
 (defn- auth-identity-handler [{:keys [identity]}]
   {:status 200, :headers {}, :body identity})
 
 (deftest test-authentication
-  (let [middleware (ig/init-key ::buddy/authentication
-                                {:backend :basic
-                                 :realm   "Test"
-                                 :authfn  test-authfn})
-        handler    (middleware auth-identity-handler)]
-    (is (= (handler (-> (mock/request :get "/")
-                        (mock/header "authorization"
-                                     "Basic QWxhZGRpbjpPcGVuU2VzYW1l")))
-           {:status 200, :headers {}, :body "Aladdin"}))
-    (is (= (handler (mock/request :get "/"))
-           {:status 200, :headers {}, :body nil}))))
+  (testing "basic authentication"
+    (let [middleware (ig/init-key ::buddy/authentication
+                                  {:backend :basic
+                                   :realm   "Test"
+                                   :authfn  test-basic-authfn})
+          handler    (middleware auth-identity-handler)]
+      (is (= (handler (-> (mock/request :get "/")
+                          (mock/header "authorization"
+                                       "Basic QWxhZGRpbjpPcGVuU2VzYW1l")))
+             {:status 200, :headers {}, :body "Aladdin"}))
+      (is (= (handler (mock/request :get "/"))
+             {:status 200, :headers {}, :body nil}))))
+
+  (testing "token authentication"
+    (let [middleware (ig/init-key ::buddy/authentication
+                                  {:backend :token
+                                   :authfn  test-token-authfn})
+          handler    (middleware auth-identity-handler)]
+      (is (= (handler (-> (mock/request :get "/")
+                          (mock/header "authorization"
+                                       "Token abcedef0123456789")))
+             {:status 200, :headers {}, :body "abcedef0123456789"}))
+      (is (= (handler (mock/request :get "/"))
+             {:status 200, :headers {}, :body nil}))))
+
+  (testing "session authentication"
+    (let [middleware (ig/init-key ::buddy/authentication {:backend :session})
+          handler    (middleware auth-identity-handler)]
+      (is (= (handler (-> (mock/request :get "/")
+                          (assoc-in [:session :identity] "Alice")))
+             {:status 200, :headers {}, :body "Alice"}))
+      (is (= (handler (mock/request :get "/"))
+             {:status 200, :headers {}, :body nil}))))
+
+  (testing "jws authentication"
+    (let [middleware (ig/init-key ::buddy/authentication
+                                  {:backend :jws
+                                   :secret  "secret"})
+          token      (jwt/sign {:user "Bob"} "secret")
+          handler    (middleware auth-identity-handler)]
+      (is (= (handler (-> (mock/request :get "/")
+                          (mock/header "authorization" (str "Token " token))))
+             {:status 200, :headers {}, :body {:user "Bob"}}))
+      (is (= (handler (mock/request :get "/"))
+             {:status 200, :headers {}, :body nil}))))
+
+  (testing "jws authentication"
+    (let [secret     "secretsecretsecretsecretsecretse"
+          middleware (ig/init-key ::buddy/authentication
+                                  {:backend :jwe
+                                   :secret  secret})
+          token      (jwt/encrypt {:user "Carol"} secret)
+          handler    (middleware auth-identity-handler)]
+      (is (= (handler (-> (mock/request :get "/")
+                          (mock/header "authorization" (str "Token " token))))
+             {:status 200, :headers {}, :body {:user "Carol"}}))
+      (is (= (handler (mock/request :get "/"))
+             {:status 200, :headers {}, :body nil})))))
